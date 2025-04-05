7ae2c1093511cce09c0e26c0d3d84e81c2b13ec3
Melese Ayichlie <119291558+meleayi@users.noreply.github.com>
2025-04-05 10:13:53 +0300

    resolve headerbox props mismaching

--- src/components/HeaderBox.tsx
+++ src/components/HeaderBox.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const HeaderBox = ({ type, title, user, subtitle }: HeaderBoxProps) => {
+const HeaderBox = ({
+  type,
+  title,
+  user,
+  subtitle,
+}: {
+  type: string;
+  title: string;
+  user: string;
+  subtitle: string;
+}) => {
   return (
     <div className="flex flex-col gap-1">
       <h1 className="text-[24px] lg:text-[30px] font-semibold text-gray-900">
